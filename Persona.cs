ddbd3f88559118e87a1c176629a31fbe2863dd1c
forna <forna@MIRABEL>
2024-01-30 16:01:43 +0100

    Aggiungere i file di progetto.

---
+++ Persona.cs
@@ -0,0 +1,24 @@
+﻿namespace ClassPersona
+{
+    public class Persona
+    {
+        private string nome;
+        private string cognome;
+        private int eta;
+
+        public Persona(string nome, string cognome, int eta)
+        {
+            this.nome = nome;
+            this.cognome = cognome;
+            this.eta = eta;
+        }
+        public string getNome() { return nome; }
+        public string getCognome() { return cognome; }
+        public int getEta() { return eta; }
+        public string getDettagli()
+        {
+            return "nome: " + nome + "\tcognome: " + cognome + "\tetà: " + eta + " anni";
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using ClassPersona;
+using System;
+
+namespace BE_U1_W1_D2
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+
+            char letteraCasuale = (char)('A' + new Random().Next(26));
+            int numeroCasuale = new Random().Next(100);
+            string benvenuto = "Benvenuto Soggetto " + letteraCasuale + numeroCasuale;
+            Console.WriteLine(benvenuto);
+            Console.WriteLine("Qual'è il tuo nome? ");
+
+            string nomePersona = Console.ReadLine();
+
+            Console.WriteLine(nomePersona + " Qual'è il tuo cognome?  ");
+
+            string cognomePersona = Console.ReadLine();
+
+            Console.WriteLine("ciao " + nomePersona + " " + cognomePersona + " Qual'è la tua età?  ");
+
+            int etaPersona = Convert.ToInt32(Console.ReadLine());
+
+            Persona soggetto1 = new Persona(nomePersona, cognomePersona, etaPersona);
+
+            Console.WriteLine("Scheda soggetto" + letteraCasuale + numeroCasuale);
+            Console.WriteLine(soggetto1.getDettagli());
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
